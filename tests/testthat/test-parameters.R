34ca8e00bde71c9a2f9b03ae766fce047123d375
Joshua Bauman <Joshua.Bauman@vai.org>
2019-07-18 14:50:57 +0000

    Updated the userguide and included an entry for correct_baseline()

--- tests/testthat/test-parameters.R
+++ tests/testthat/test-parameters.R
@@ -67,8 +67,8 @@ test_that("Max Rates per Baseline can be calculated", {
 
 test_that("Max Rates per peak height can be calculated", {
   data(ca_flux)
-  rates <- max_rates_ph(ca_flux$Mean4)
-  expect_equal(round(rates[1], 3), 0.388)
   rates <- max_rates_ph(ca_flux$Mean1)
   expect_equal(round(rates[1], 3), 0.236)
+  rates <- max_rates_ph(ca_flux$Mean4)
+  expect_equal(round(rates[1], 3), 0.388)
 })
