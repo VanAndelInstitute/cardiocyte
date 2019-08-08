05b1ba670484874ec9e549fa1a399441ba307ef5
Joshua Bauman <Joshua.Bauman@vai.org>
2019-08-08 19:22:17 +0000

    added p param to peak_height()

--- R/peak_height.R
+++ R/peak_height.R
@@ -3,6 +3,7 @@
 #' Calculate the heights of peaks in ensemble matrix
 #'
 #' @param x vector of trace data
+#' @param p percent of max to limit noisy peaks
 #'
 #' @return vector containing the peak heights of the ensemble matrix
 #' @export
@@ -11,12 +12,13 @@
 #' data(ca_flux)
 #' dat <- ensemble(ca_flux$Mean1)
 #' ph <- peak_height(dat)
-peak_height <- function(x) {
+peak_height <- function(x, p = 0) {
   # find the location of the peaks
     peaks <- find_peaks(x)
       # find the baseline for each peak
     bl <- baseline(as.matrix(t(x)), method = 'rolling', wm = 5, ws = 5)
       # subtract the baseline from the peak value
     heights <- x[peaks] - bl@baseline[peaks]
+    heights <- heights[which(heights > p*max(heights))]
     return(heights)
 }
