68d944db2c47416da8e3f980388d54dec86fb402
JoshBauman <joshua.bauman@vai.org>
2020-06-01 18:11:22 +0000

    fixed bug where peak_vels function returned velocities twice added more parameters to peak_vels

--- R/peak_vels.R
+++ R/peak_vels.R
@@ -4,29 +4,34 @@
 #'
 #' @param x a vector of data
 #' @param downward.vel specifies if you want upward or downward velocities. Specify "TRUE" for downward.
+#' @param p a percentage of the highest peak with which to limit the results (in the case of lots of noisy peaks). Default is 0.
+#' @param offset Move the sliding window offset time points  if desired
+#' @param norm Should each pulse be normalized to [0,1]?
+#' @param ... additional parameters to pass to find_peaks
+#'
 #'
 #' @importFrom stats smooth.spline predict
 #' @export
 #'
-peak_vels <- function(x, downward.vel = FALSE) {
-  storage <- numeric(nrow(ensemble(x)))
-  for (i in 1:nrow(ensemble(x))) {
-    storage[i] <- peak_vel(x, peak.number=i, downward.vel = downward.vel)
+peak_vels <- function(x, downward.vel = FALSE, p=0, offset = 0, norm = FALSE, ...) {
+  storage <- numeric(nrow(ensemble(x, p=p, offset = offset, norm = norm, ...)))
+  for (i in 1:nrow(ensemble(x, p=p, offset = offset, norm = norm, ...))) {
+    storage[i] <- peak_vel(x, peak.number=i, downward.vel = downward.vel, p=p, offset = offset, norm = norm, ...)
   }
   return(storage)
 }
 
 
 # internal function to find the velocity of each peak
-peak_vel <- function(x, peak.number = 1, downward.vel = FALSE){
-  ens <- ensemble(x)
+peak_vel <- function(x, peak.number = 1, downward.vel = FALSE, p=0, offset = 0, norm = FALSE, ...){
+  ens <- ensemble(x, p=p, offset=offset, norm=norm, ...)
   peak <- ens[peak.number,]
   smooth.peak <- smooth.spline(peak)
   derivs <- predict(smooth.peak, deriv=1)
   down <- downward.vel
   if(down == TRUE) {
-    print(min(derivs$y))
+    return(min(derivs$y))
   } else {
-    print(max(derivs$y))
+    return(max(derivs$y))
   }
 }
